57afc24f9b3f8b8c7cdf03ca6156c18b064d5d08
Nikky INTHASOT <ninthasot@divotiz.com>
2025-07-10 09:25:26 +0200

    dotnet project

---
+++ app/ProjectBoardApiDotNet/src/Api/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+//Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+await app.RunAsync();
