7ce70f41af9ef16f48645ec65cf1f73dca922cae
Nikky <nikkyinthasot@gmail.com>
2025-07-12 17:59:51 +0200

    telemetry + serilog fix duplicate

--- app/ProjectBoardApiDotNet/src/Api/Program.cs
+++ app/ProjectBoardApiDotNet/src/Api/Program.cs
@@ -8,18 +8,46 @@ using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure Serilog for structured logging
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(builder.Configuration)
-    .WriteTo.Console(formatProvider: null)
-    .CreateLogger();
+builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration));
 
-builder.Host.UseSerilog();
+var otlpEndpoint = builder.Configuration["OpenTelemetry:OtlpEndpoint"];
+if (string.IsNullOrWhiteSpace(otlpEndpoint))
+    throw new InvalidOperationException("OpenTelemetry:OtlpEndpoint configuration is missing.");
+
+builder
+    .Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+    .WithTracing(tracing =>
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri(otlpEndpoint); // Container name and OTLP port
+            })
+    )
+    .WithMetrics(metrics =>
+        metrics
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri(otlpEndpoint!);
+            })
+    );
 
 builder.AddDataBase();
+
 builder.Services.AddControllers();
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+Log.Information("OpenTelemetry has been initialized and configured.");
+
+Log.Debug("Test OpenTelemetry log");
 
 if (app.Environment.IsDevelopment())
 {
